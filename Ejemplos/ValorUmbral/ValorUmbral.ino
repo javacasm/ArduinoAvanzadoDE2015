715e6f738e2cb74f4aa10c50a48942f79bb51e3f
javacasm <javacasm@gmail.com>
2015-07-14 16:58:36 +0200

    umbral

--- Ejemplos/ValorUmbral/ValorUmbral.ino
+++ Ejemplos/ValorUmbral/ValorUmbral.ino
@@ -1,26 +1,34 @@
-int pinLedRojo=6, pinLedVerde=5;
-
+int pinLedRojo=6, pinLedAmarillo=4, pinLedVerde=5;
 void setup() {
 pinMode(pinLedRojo,OUTPUT);
 pinMode(pinLedVerde,OUTPUT);
+pinMode(pinLedAmarillo,OUTPUT);
 Serial.begin(9600);
 }
-
 void loop() {
 int valor=analogRead(A0);
-  if ( valor < 500 )
-  {
-      // valor inferior a 500
+  if ( valor < 300 )
+  {   // valor inferior a 300
       digitalWrite(pinLedVerde,HIGH);
       digitalWrite(pinLedRojo,LOW);
+      digitalWrite(pinLedAmarillo,LOW);
       Serial.println("Valor Bajo");
   }
-  else
-  {
-      // valor superior a 500
+
+  if( valor>600)
+  {   // valor superior a 600
       digitalWrite(pintLedRojo,HIGH);
       digitalWrite(pinLedVerde,LOW);
+      digitalWrite(pinLedAmarillo,LOW);
       Serial.println("Valor Alto");
+  }
+
+  if( (valor>300) && (valor<600)        )
+  {   // valor superior a 300 y menor a 600
+      digitalWrite(pintLedRojo,LOW);
+      digitalWrite(pinLedVerde,LOW);
+      digitalWrite(pinLedAmarillo,HIGH);
+      Serial.println("Valor Medio");
   }
   delay(200);
 }

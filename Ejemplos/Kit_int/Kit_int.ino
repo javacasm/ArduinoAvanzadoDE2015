325c1eeb273573e2504da859cd3165405b3ad8de
javacasm <javacasm@gmail.com>
2015-07-15 20:33:49 +0200

    kit

---
+++ Ejemplos/Kit_int/Kit_int.ino
@@ -0,0 +1,20 @@
+#define PAUSA 100
+void setup() {  DDRB=B00111111; // 63
+ DDRD=B11110000;
+ }
+ void pinta(int valor)
+{
+  int parteAltaValor=valor/256; // El cociente
+  int parteBajaValor=valor%256; // El resto
+  PORTD=parteBajaValor;
+  PORTB=parteAltaValor;
+}
+void loop() {
+  int valor=B10000; // los enteros tiene 2 bytes o 16 bits
+  for(int i=0;i<10;i++)
+  { pinta(valor); delay(PAUSA); valor=valor<<1;
+  }
+  for(int i=0;i<10;i++)
+  { pinta(valor); delay(PAUSA); valor=valor>>1;
+  }
+}
